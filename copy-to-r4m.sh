7f50c121c5a10eb08016c637203621b103c1e369
Joe Lauer <joe@lauer.bz>
2014-02-26 12:32:12 -0800

    Fix for Android 2.3 NPE bug; Fix for Kindle HDX

---
+++ copy-to-r4m.sh
@@ -0,0 +1,3 @@
+VERSION="0.9.4-SNAPSHOT"
+cp library/build/libs/library-$VERSION.aar ../android-r4m/r4m/libs/actionbar-pulltorefresh-library-$VERSION.aar 
+cp extras/actionbarsherlock/build/libs/extras/actionbarsherlock-$VERSION.aar ../android-r4m/r4m/libs/actionbar-pulltorefresh-extras-abs-$VERSION.aar
